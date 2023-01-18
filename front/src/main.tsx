146d2b1fc64c18a69bbeb38fc8d15e987320dc3c
Jaime Burbano <burbanojaime98@gmail.com>
2023-01-18 17:18:51 -0500

    fix: Setup fixed

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,8 +1,10 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/App'
+import './assets/styles/index.css'
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
+      { find: '@utils', replacement: '/src/utils' },
       { find: '@atoms', replacement: '/src/UI/atoms' }
     ]
   },
-  build: {
-    sourcemap: true,
-  },
-});
+})
