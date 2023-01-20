761ef7ea40f218c32cdc10ebebf860e645ab46b1
Jaime Burbano <burbanojaime98@gmail.com>
2023-01-19 20:46:28 -0500

    fix: clean up

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,13 +3,25 @@ import './assets/styles/index.css';
 import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './pages/App';
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: import.meta.env.PROD,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
--- front/src/pages/App.tsx
+++ front/src/pages/App.tsx
@@ -1,21 +1,26 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
 
-import Main from './Main';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: import.meta.env.PROD,
-    },
-  },
-});
+import { useGetShifts } from '../hooks/useShifts';
+import Button from '../UI/atoms/Button';
+import SetShiftAssignmentModal from '../UI/organisms/SetShiftAssignmentModal';
+import ShiftsTable from '../UI/organisms/ShiftsTable';
 
 const App = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const { data, isLoading, isError, error } = useGetShifts();
+
+  if (isLoading && !isError) return <p>Loading...</p>;
+  if (isError && error) return <p>Sorry. {error?.message}</p>;
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Main />
-    </QueryClientProvider>
+    <main className="w-full max-w-5xl m-auto p-12 box-border flex flex-col gap-8">
+      <header className="w-full flex justify-between items-center">
+        <h1 className="text-lg font-medium leading-6 text-gray-900">Shifts</h1>
+        <Button onClick={() => setOpenModal(true)}>Set Shift Assignment</Button>
+      </header>
+      <ShiftsTable data={data} />
+      <SetShiftAssignmentModal open={openModal} onClose={() => setOpenModal(false)} />
+    </main>
   );
 };
 
